6fd7456d9b2cee3f87ae4f2950dd800d94c8d496
25Emmanuel <ikwumonuemmanuel@gmail.com>
2022-08-04 17:18:23 +0100

    question elements gets removed after its been displayed

---
+++ check.js
@@ -0,0 +1,1 @@
+console.log(Object.entries(array[0].option));
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var array = [
-    {question: "what is the sum of 2 + 3", id: 1},
-    {question: "what is the square-root of 25", id: 2},
-    {question: "what is the 25 degree Celsius in Fahrenheit", id: 3},
-    {question: "what is the sides a polygon has?", id: 4},
-    {question: "what is the freezing point of water in Celsius", id: 5},
-    {question: "what is the boiling point of water in Celsius", id: 6},
-    {question: "How many Inches makes a foot", id: 7},
-    {question: "what is the formula to find the Area of a Square", id: 8},
-    {question: "How many Meters makes 1 Kilometer", id: 9},
-    {question: "what is the sum of 2 + 3", id: 10}
+    {question: "what is the sum of 2 + 3", id: 0, ans: "5"},
+    {question: "what is the square-root of 25", id: 1, ans: "5"},
+    {question: "what is the 25 degree Celsius in Fahrenheit", id: 2, ans: "77"},
+    {question: "what is the sides a polygon has?", id: 3, ans: "5"},
+    {question: "what is the freezing point of water in Celsius", id: 4, ans: "0"},
+    {question: "what is the boiling point of water in Celsius", id: 5, ans: "100"},
+    {question: "How many Inches makes a foot", id: 6, ans: "12"},
+    {question: "what is the formula to find the Area of a Square", id: 7, ans: "L*B"},
+    {question: "How many Meters makes 1 Kilometer", id: 8, ans: "1000"},
+    {question: "what is the Square root of 81", id: 9, ans: "9"}
 ]
-// console.log(Object.entries(array[0].option));
+
 
 var main = document.querySelector("div");
 var button = document.querySelector("button")
@@ -20,44 +20,118 @@ var corrector = document.querySelector("#corrector")
 var input = document.querySelector(".enter")
 var retry = document.querySelector("#re-try")
 
+
+
+var random_number;
+var new_question_selector;
+var new_question;
+var score = 0;
+function randomNumAssign () {
+    random_number = Math.floor(Math.random() * (array.length));
+    console.log(`this random ${random_number}`);
+} 
+randomNumAssign()
+var counter = 0;
+
+var test_ans = array[random_number].ans;
+
+testp.innerHTML = array[random_number].question;
+
 const tester = () => {
-    var i = document.createElement("i")
-    console.log("working-3");
-    i.classList.add("fa-solid", "fa-check", "correct")
-    console.log("working-4!!!");
+    
+    
     
     if (input.value === "") {
         alert("you have not entered any answer..")
-    } else if (input.value !== "4") {
-        console.log("working");
+    } else if (counter === 0 && input.value !== test_ans) {
+        
         retry.style.display = "block"
-        console.log("still working");
+        
+        counter++
+        setInterval(()=> {retry.style.display = "none"}, 3000)
+        
+        input.value = ''
+       
+        
+        // test
+        array.splice(random_number, 1)
+        new_question_selector = Math.floor(Math.random() * (array.length))
+        new_question = array[(new_question_selector)].question;
+        testp.innerHTML = new_question;
+
+        console.log(array);
+    } 
+    else if (counter > 0 && input.value !== array[new_question_selector].ans) {
+        retry.style.display = "block"
         setInterval(()=> {retry.style.display = "none"}, 3000)
         input.value = ''
+        
+        counter++
+
+        array.splice(new_question_selector, 1)
+        new_question_selector = Math.floor(Math.random() * (array.length))
+        new_question = array[(new_question_selector)].question;
+        testp.innerHTML = new_question;
+
+        console.log(array);
     }
     else {
         corrector.style.display = "block"
         setInterval(()=> {corrector.style.display = "none"}, 3000)
-        nextQuestion()
+        
+        
+        if (counter === 0) {
+            array.splice(random_number, 1)
+            new_question_selector = Math.floor(Math.random() * (array.length))
+            new_question = array[new_question_selector].question;
+            testp.innerHTML = new_question;
+           
+           
+        }
+
+
+        else {
+            array.splice(new_question_selector, 1)
+            new_question_selector = Math.floor(Math.random() * (array.length))
+            new_question = array[(new_question_selector)].question;
+            testp.innerHTML = new_question;
+        }       
+        
         input.value = ''
+           
+        console.log(array);
+          
+        score++
+        counter++;
     }
+    
+    console.log(`final score: ${score}`);
+    console.log(`final counter: ${counter}`);
 }
-function nextQuestion () {
-    if (input.value === "") {
-        alert("the input field is empty")
-    } else if (input.value != "4") {
-        retry.style.display = "block"
-        setInterval(()=> {retry.style.display = "none"}, 3000)
+
+
+function next () {
+    if (counter === 0) {
+        array.splice(random_number, 1)
+        new_question_selector = Math.floor(Math.random() * (array.length))
+        new_question = array[(new_question_selector)].question;
+        testp.innerHTML = new_question;
+
+        console.log(array);
         input.value = ''
     }
-    else {
-        // var text = document.createTextNode(array[0].question);
-        testp.textContent = array[0].question;
-        corrector.style.display = "block"
-        setInterval(()=> {corrector.style.display = "none"}, 3000)
+    if (counter > 0) {
+        array.splice(new_question_selector, 1)
+        new_question_selector = Math.floor(Math.random() * (array.length))
+        new_question = array[(new_question_selector)].question;
+        testp.innerHTML = new_question;
+
+        console.log(array);
         input.value = ''
     }
-    console.log('working');
+    counter++
+    console.log(`final score: ${score}`);
+    console.log(`final counter: ${counter}`);
 }
-button.addEventListener("click", nextQuestion)
+button.addEventListener("click", next)
 pbutton.addEventListener("click", tester)
